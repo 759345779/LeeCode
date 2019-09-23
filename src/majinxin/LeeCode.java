9dcca1d9eb8852bfd1942683d1b9918764a18b8d
majinxin <majinxin1@kingsoft.com>
2019-09-23 10:12:51 +0800

    2.  Add Two Numbers

--- src/majinxin/LeeCode.java
+++ src/majinxin/LeeCode.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
+import static majinxin.MethodSet.run_02;
+import static majinxin.MethodSet.run_02_printListNode;
+
 public class LeeCode {
     /**难度: Easy
      * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
@@ -33,6 +36,61 @@ public class LeeCode {
 
         }
 
+
+    }
+
+    /**
+     * 2. Add Two Numbers
+     * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+     * <p>
+     * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+     * <p>
+     * Example:
+     * <p>
+     * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+     * Output: 7 -> 0 -> 8
+     * Explanation: 342 + 465 = 807.
+     */
+
+    @Test
+    public void test_leeCode_02() {
+        MethodSet.ListNode node_01 = new MethodSet.ListNode<>(3, null);
+        MethodSet.ListNode node_02 = new MethodSet.ListNode<>(4, node_01);
+        MethodSet.ListNode<Integer> input_01 = new MethodSet.ListNode<>(2, node_02);
+
+        MethodSet.ListNode n_01 = new MethodSet.ListNode<>(4);
+        MethodSet.ListNode n_02 = new MethodSet.ListNode<>(6,n_01);
+        MethodSet.ListNode<Integer> input_02 = new MethodSet.ListNode<>(5, n_02);
+
+        run_02_printListNode(input_01);
+        run_02_printListNode(input_02);
+        MethodSet.ListNode outputListNode = run_02(input_01, input_02);
+        run_02_printListNode(outputListNode);
+
+    }
+
+    /**
+     * Given a string, find the length of the longest substring without repeating characters.
+     *
+     * Example 1:
+     *
+     * Input: "abcabcbb"
+     * Output: 3
+     * Explanation: The answer is "abc", with the length of 3.
+     * Example 2:
+     *
+     * Input: "bbbbb"
+     * Output: 1
+     * Explanation: The answer is "b", with the length of 1.
+     * Example 3:
+     *
+     * Input: "pwwkew"
+     * Output: 3
+     * Explanation: The answer is "wke", with the length of 3.
+     *              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+     */
+    @Test
+    public void test_leeCode_03() {
 
     }
 }
--- src/majinxin/MethodSet.java
+++ src/majinxin/MethodSet.java
@@ -44,4 +44,78 @@ public class MethodSet {
         return new int[]{0, 0};
 
     }
+
+    public static class ListNode<T>{
+        T value;
+        ListNode<T> nextNode;
+
+        public ListNode(T value) {
+            this(value, null);
+        }
+
+        public ListNode(T value, ListNode<T> nextNode) {
+            this.value = value;
+            this.nextNode = nextNode;
+        }
+
+        boolean hasNext() {
+            return nextNode != null;
+        }
+
+        public ListNode<T> next() {
+            return nextNode;
+        }
+
+
+    }
+    public static ListNode<Integer> run_02(ListNode<Integer> input_01, ListNode<Integer> input_02) {
+        ListNode<Integer> r_node = null;
+        ListNode<Integer> returnNode = null;
+        int tmp = 0;
+        while (input_01 !=null || input_02 !=null) {
+            if (input_01 !=null) {
+                tmp = tmp + input_01.value;
+                input_01 = input_01.nextNode;
+            }
+            if (input_02 !=null) {
+                tmp = tmp + input_02.value;
+                input_02 = input_02.nextNode;
+            }
+
+            ListNode node = null;
+            if (tmp > 9) {
+                node = new ListNode(tmp % 10);
+                tmp = 1;
+            }else {
+                node = new ListNode(tmp);
+                tmp = 0;
+            }
+
+            System.out.println("node=" + node.value);
+            if (returnNode == null) {
+                r_node = node;
+                returnNode = r_node;
+            }else {
+                r_node.nextNode = node;
+                r_node = node;
+            }
+        }
+        return returnNode;
+    }
+    public static void run_02_printListNode(ListNode<Integer> input){
+        StringBuilder builder = new StringBuilder("[");
+        ListNode<Integer> tmp = input;
+        while (tmp!=null) {
+            builder.append(tmp.value);
+            builder.append(", ");
+            tmp = tmp.nextNode;
+        }
+        int lastIndex = builder.lastIndexOf(",");
+        if (lastIndex >= 0) {
+            builder.delete(lastIndex, builder.length());
+        }
+        builder.append("]");
+        System.out.println(builder.toString());
+
+    }
 }
